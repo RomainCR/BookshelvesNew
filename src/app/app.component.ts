c3c878cec083c47923f708903485592bea51b20a
RomainCR <romain.crey@gmail.com>
2019-08-16 21:39:25 +0200

    v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import 'firebase/auth'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyDgckKEN7Sd4EYV9FbLXdT2yCg0Dure5K0",
+      authDomain: "bookshelvesoc-12010.firebaseapp.com",
+      databaseURL: "https://bookshelvesoc-12010.firebaseio.com",
+      projectId: "bookshelvesoc-12010",
+      storageBucket: "bookshelvesoc-12010.appspot.com",
+      messagingSenderId: "945589189492",
+      appId: "1:945589189492:web:92c597545c9b6529"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
